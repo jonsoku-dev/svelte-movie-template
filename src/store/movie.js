dc0524074e1ea1487624a004accc49bba9fc46a4
jonsoku2 <whdtjr2792@gmail.com>
2021-03-06 10:37:03 +0900

    11. 응답 에러 출력과 movie.js 리팩토링

--- src/store/movie.js
+++ src/store/movie.js
@@ -7,6 +7,7 @@ const OBDB_API_KEY = '9b09ed9e'
 export const movies = writable([])
 export const theMovie = writable({})
 export const loading = writable(false)
+export const message = writable('Search for the movie title!')
 
 export async function searchMovies(payload) {
     if (get(loading)) return
@@ -14,21 +15,32 @@ export async function searchMovies(payload) {
 
     const { title, type, year, number } = payload
 
-    const res = await axios.get(
-        `http://www.omdbapi.com/?apikey=${OBDB_API_KEY}&s=${title}&type=${type}&y=${year}`
-    )
+    let total = 0
 
-    const { Search, totalResults } = res.data
-    movies.set(Search)
+    try {
+        const res = await _fetchMovie({
+            ...payload,
+            page: 1,
+        })
+        const { Search, totalResults } = res.data
+        movies.set(Search)
+        total = totalResults
+    } catch (msg) {
+        movies.set([])
+        message.set(msg)
+        loading.set(false)
+        return
+    }
 
-    const pageLength = Math.ceil(totalResults / 10)
+    const pageLength = Math.ceil(total / 10)
 
     if (pageLength > 1) {
         for (let page = 2; page <= pageLength; page += 1) {
             if (page > number / 10) break
-            const res = await axios.get(
-                `http://www.omdbapi.com/?apikey=${OBDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-            )
+            const res = await _fetchMovie({
+                ...payload,
+                page,
+            })
             const { Search } = res.data
             movies.update(($movies) => {
                 return _unionBy($movies, Search, 'imdbID') // 중복 제거
@@ -43,10 +55,35 @@ export async function searchMovieWithId(id) {
     if (get(loading)) return
     loading.set(true)
 
-    const res = await axios.get(
-        `http://www.omdbapi.com/?apikey=${OBDB_API_KEY}&i=${id}&plot=full`
-    )
+    try {
+        const res = await _fetchMovie({ id })
+        theMovie.set(res.data)
+        console.log(res.data)
+        loading.set(false)
+    } catch (msg) {
+        movies.set([])
+        message.set(msg)
+        loading.set(false)
+    }
+}
 
-    theMovie.set(res.data)
-    loading.set(false)
+function _fetchMovie(payload) {
+    const { title, type, year, page, id } = payload
+    const url = id
+        ? `http://www.omdbapi.com/?apikey=${OBDB_API_KEY}&i=${id}&plot=full`
+        : `http://www.omdbapi.com/?apikey=${OBDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await axios.get(url)
+            console.log(res.data)
+            if (res.data.Error) {
+                reject(res.data.Error)
+            }
+            resolve(res)
+        } catch (e) {
+            console.log(e.response.status)
+            reject(error.message)
+        }
+    })
 }
